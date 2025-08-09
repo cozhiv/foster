6baaf6918eb3a3968e125006c8b002e295af8eb5
cozhiv <cozhiv@gmail.com>
2025-08-09 13:45:33 +0300

    Pre Budget func

--- pages/api/lists/[listId]/create.ts
+++ pages/api/lists/[listId]/create.ts
@@ -1,26 +1,27 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]";
-import { prisma } from "@/lib/prisma";
-//import { RequestData } from "next/dist/server/web/types";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "../../auth/[...nextauth]";
+// import { prisma } from "@/lib/prisma";
+// //import { RequestData } from "next/dist/server/web/types";
 
-export default async function handler(req: any, res) {
-  const session: any = await getServerSession(req, res, authOptions);
-  if (!session) return res.status(401).json({ error: "Unauthorized" });
+// export default async function handler(req: any, res) {
+//   const session: any = await getServerSession(req, res, authOptions);
+//   if (!session) return res.status(401).json({ error: "Unauthorized" });
 
-  if (req.method === "POST") {
-    const { name } = req.body;
-    const user = await prisma.user.findUnique({ where: { email: session.user.email } });
-    const list = await prisma.list.create({
-      data: {
-        status:"why",
-        name,
-        users: {
-          create: [{ userId: user.id }],
-        },
-      },
-    });
-    return res.status(201).json(list);
-  }
+//   if (req.method === "POST") {
+//     const { name, budget } = req.body;
+//     const user = await prisma.user.findUnique({ where: { email: session.user.email } });
+//     const list = await prisma.list.create({
+//       data: {
+//         status:"why",
+//         name,
+//         users: {
+//           create: [{ userId: user.id }],
+//         },
+//         budget
+//       },
+//     });
+//     return res.status(201).json(list);
+//   }
 
-  res.status(405).end();
-}
+//   res.status(405).end();
+// }
